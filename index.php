6184032829bf19238f6269e53cae46707dab1354
SNYdev <yanis.sahnoune@supinternet.fr>
2015-05-09 10:44:36 +0200

    website 1.0

--- index.php
+++ index.php
@@ -58,17 +58,18 @@
     <head>
         <link rel="stylesheet" type="text/css" href="css/reset.css">
         <link rel="stylesheet" type="text/css" href="css/style.css">
-        <script src="js/jquery-2.1.3.min.js"></script>
-        <script src="jquery-ui.js"></script>
+        <script type="text/javascript" src="js/jquery-2.1.3.min.js"></script>
+        <script type="text/javascript" src="js/jquery-ui.js"></script>
+        <script type="text/javascript" src="js/jquery.carouFredSel-6.2.1.js"></script>
         <title>Yanis Sahnoune - CV 2015</title>
     </head>
     <body>
         <nav id="nav-left">
             <div class="navigation" id="info-nav-left">
                 <ul id="ul-nav">
-                    <li class="nav-title" id="accueil"><a href="www.facebook.fr" class="link-nav" id="link-accueil"><img src="img/icone-accueil.png" class="icone-nav">Accueil</a></li>
-                    <li class="nav-title" id="travaux"><a href="" class="link-nav" id="link-work"><img src="img/icone-travaux.png" class="icone-nav">Travaux</a></li>
-                    <li class="nav-title" id="contact"><a href="" class="link-nav" id="link-contact"><img src="img/icone-contact.png" class="icone-nav">Contact</a></li>
+                    <li class="nav-title" id="accueil"><a href="www.facebook.fr" class="link-nav" id="link-accueil"><img src="css/img/icone-accueil.png" class="icone-nav">Accueil</a></li>
+                    <li class="nav-title" id="travaux"><a href="" class="link-nav" id="link-work"><img src="css/img/icone-travaux.png" class="icone-nav">Travaux</a></li>
+                    <li class="nav-title" id="contact"><a href="" class="link-nav" id="link-contact"><img src="css/img/icone-contact.png" class="icone-nav">Contact</a></li>
                 </ul>
             </div>
             <div class="socialNtework" id="social-nav-left">
@@ -76,29 +77,8 @@
                 <a class="link-socialNetwork" href=""><div class="socialNet" id="gitHub"><p class="txt-social-network">Git<p></div></a>
             </div>
         </nav>
-    <?php
-
-
-        // Affiche les flashBag : des messages informatif du genre "votre message a bien Ã©tÃ© envoyÃ©"
-        // if (isset($_SESSION['flashBag'])) {
-        //     foreach ($_SESSION['flashBag'] as $type => $flash) {
-        //         foreach ($flash as $key => $message) {
-        //             echo '<div class="'.$type.'" role="'.$type.'" >'.$message.'</div>';
-        //             // un fois affichÃ© le message doit Ãªtre supprimÃ©
-        //             unset($_SESSION['flashBag'][$type][$key]);
-        //         }
-        //     }
-        // }
-
-        // // Charge la page demandÃ©e
-        // $fileController = 'page/'.$routing[$page]['controller'].'.php';
-        // if (file_exists($fileController)) {
-        //     require $fileController;
-        // } else {
-        //     echo 'File is missing';
-        // }
-
-        // die;
-    ?>
-    </body>
+        <div class="contener" id="right">
+            <div class="content">
+            </div>
+        </div>
 </html>
