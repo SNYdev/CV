131ad3ab9bf6195634bae02ae460842f95edeb9d
SNYdev <yanis.sahnoune@supinternet.fr>
2015-05-15 13:06:51 +0200

    add content in competence dev

--- index.php
+++ index.php
@@ -69,12 +69,12 @@
                 <ul id="ul-nav">
                     <li class="nav-title" id="accueil"><a href="www.facebook.fr" class="link-nav" id="link-accueil"><img src="css/img/icone-accueil.png" class="icone-nav">Accueil</a></li>
                     <li class="nav-title" id="travaux"><a href="" class="link-nav" id="link-work"><img src="css/img/icone-travaux.png" class="icone-nav">Travaux</a></li>
-                    <li class="nav-title" id="contact"><a href="" class="link-nav" id="link-contact"><img src="css/img/icone-contact.png" class="icone-nav">Contact</a></li>
+                    <li class="nav-title" id="contact"><a href="#dev" class="link-nav" id="link-contact"><img src="css/img/icone-contact.png" class="icone-nav">Contact</a></li>
                 </ul>
             </div>
             <div class="socialNtework" id="social-nav-left">
                 <a class="link-socialNetwork" href="https://www.linkedin.com/profile/view?id=397558767&trk=nav_responsive_tab_profile_pic"><div class="socialNet" id="linkedIn"><p class="txt-social-network">In<p></div></a>
-                <a class="link-socialNetwork" href=""><div class="socialNet" id="gitHub"><p class="txt-social-network">Git<p></div></a>
+                <a class="link-socialNetwork" href="https://github.com/SNYdev"><div class="socialNet" id="gitHub"><p class="txt-social-network">Git<p></div></a>
             </div>
             <div class="draWork">
                 <h1 class="creationNavLeft">CREATIONS</h1>
@@ -88,6 +88,7 @@
                     </div>
                 </div>
             </div>
+            <input type="color" id="changeColor">
         </nav>
         <div class="contener" id="right">
             <div class="main">
@@ -98,19 +99,19 @@
                     <p class="info-txt" id="phone">06 16 22 06 93</p>
                 </div>
                 <div class="formation-block">
-                    <div class="Fblock" id="left">
+                    <div class="Fblock" id="Fleft">
                         <h3 class="title-formation">2014/2017</h3>
-                        <p class="content-txt-formation">Bachelor en developpement des technologies du web @ sup</p>
+                        <p class="content-txt-formation">Bachelor en developpement des technologies du web @ sup'internet</p>
                     </div>
-                    <div class="Fblock" id="right">
+                    <div class="Fblock" id="Fright">
                         <h3 class="title-formation">2013/2014</h3>
                         <p class="content-txt-formation">Creation de mon auto-entreprise: Webdesign, creations</p>
                     </div>
-                    <div class="Fblock" id="left">
+                    <div class="Fblock" id="Fleft">
                         <h3 class="title-formation">2010/2013</h3>
                         <p class="content-txt-formation">Formation en alternance au CFA COM avec la société GAD</p>
                     </div>
-                    <div class="Fblock" id="right">
+                    <div class="Fblock" id="Fright">
                         <h3 class="title-formation">2009/2010</h3>
                         <p class="content-txt-formation">Formation chez Graphisme & communication de maquette</p>
                     </div>
@@ -119,6 +120,30 @@
                     <div class="title-txtDG">
                         <h2 class="title-txt" id="competences">COMPETENCES - </h2>
                         <h3 class="title-txt" id="dev-title">DEV</h3>
+                        <div class="content-competenceDev">
+                            <div class="content-txt-competenceDev" id="BlocHtml">
+                                <img src="css/img/iconeHTML.jpg" width="70px" class="title-informationContent" id="html-bloc"/>
+                                <p class="decription-infoCompetence" id="txt-html-bloc">
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt magna tincidunt sem lacinia, vel sagittis diam varius. Praesent non vestibulum felis.
+                                </p>
+                            </div>
+                            <div class="content-txt-competenceDev" id="BlocPhp">
+                                <img src="css/img/iconePHP.jpg" width="70px" class="title-informationContent" id="php-bloc">
+                                <p class="decription-infoCompetence" id="txt-php-bloc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt magna tincidunt sem lacinia, vel sagittis diam varius. Praesent non vestibulum felis.</p>
+                            </div>
+                            <div class="content-txt-competenceDev" id="BlocJavascript">
+                                <img src="css/img/iconeJS.jpg" width="70px" class="title-informationContent" id="javascript-bloc">
+                                <p class="decription-infoCompetence" id="txt-javascript-bloc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt magna tincidunt sem lacinia, vel sagittis diam varius. Praesent non vestibulum felis.</p>
+                            </div>
+                            <div class="content-txt-competenceDev" id="BlocSQL">
+                                <img src="css/img/iconeSQL.jpg" width="70px" class="title-informationContent" id="sql-bloc">
+                                <p class="decription-infoCompetence" id="txt-sql-bloc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt magna tincidunt sem lacinia, vel sagittis diam varius. Praesent non vestibulum felis.</p>
+                            </div>
+                            <div class="content-txt-competenceDev" id="BlocGit">
+                                <img src="css/img/iconeGIT.jpg" width="70px" class="title-informationContent" id="blocgit-bloc">
+                                <p class="decription-infoCompetence" id="txt-git-bloc">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis tincidunt magna tincidunt sem lacinia, vel sagittis diam varius. Praesent non vestibulum felis.</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="competences" id="graphisme">
@@ -131,7 +156,7 @@
                     <!-- à remplacer avec un formulaire de contact normal -->
                     <form class="form-horizontal" name="formInscription">
                         <fieldset>
-                        <legend>Contact</legend>
+                        <legend>inscription</legend>
                             <input id="nameUser" name="nameUser" type="text" placeholder="Saisir votre nom" class="input-xlarge">
                             <input id="firstnameUser" name="firstnameUser" type="text" placeholder="Saisir votre prenom" class="input-xlarge">
                             <input id="mailUser" name="mailUser" type="text" placeholder="Saisir votre adresse mail" class="input-xlarge">
@@ -141,37 +166,95 @@
                             <input id="verifPasswordUser" name="verifPasswordUser" type="password" placeholder="Saisir à nouveau votre password" class="input-xlarge">
                             <input id="submitButton" name="submitButton" type="submit" class="buttonSub" value="Inscription">
                         </fieldset>
-                        <div class="error-inscription">
+                        <div id="error-inscription">
                         </div>
                     </form>
                 </div>
             </div>
-            <script>
-
-            // $(function(){
-
-            //         $('.error-inscription').html('');
-            //         var name = $(this).find('input[name="nameUser"]').val();
-            //         var firstName = $(this).find('input[name="firstnameUser"').val();
-            //         var mail = $(this).find('input[name="mailUser"').val();
-            //         var location = $(this).find('input[name="locationUser"').val();
-            //         var age = $(this).find('input[name="ageUser"]').val();
-            //         var password = $(this).find('input[name="passwordUser"]').val();
-            //         var verifPass = $(this).find('input[name="verifPasswordUser"').val();
-
-            //         $('form[name="formInscription"]').submit(function(){
-
-            //         if (name == '' || name.length < 5) {
-            //             $('.error-inscription').html('Pseudo trop court')
-            //             return false
-            //         }
-
-            //     return false;
-            //     });
-            // });
-            </script>
         </div>
     </body>
+    <script>
+        $(function(){
+
+
+                function testEmail(email){
+                        var regEmail = new RegExp('^[0-9a-z._-]+@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}$','i');
+                        return regEmail.test(email);
+                }
+
+                var mail = $(this).find('input[name="mailUser"]');
+                mail.change(function () {
+                        var userEmail = this.value;
+                        if(userEmail == '' || testEmail(userEmail)){
+                            $('#error-inscription').html('');
+                        }
+                        else if(testEmail(userEmail) === false){
+                             $('#error-inscription').html('Adresse non valide');
+                        }
+                });
+
+                $('#changeColor').change(function() {
+                        $('#nav-left').animate({
+                            backgroundColor: $(this).val()
+                        }, 2000);
+                        
+                        $('.main').animate({
+                            backgroundColor: $(this).val()
+                        }, 2000);
+                });
+
+                $('form[name="formInscription"]').submit(function(){
+
+                        $('.error-inscription').html('');
+                        var name = $(this).find('input[name="nameUser"]').val();
+                        var firstName = $(this).find('input[name="firstnameUser"]').val();
+                        var mail = $(this).find('input[name="mailUser"]');
+                        var location = $(this).find('input[name="locationUser"]').val();
+                        var age = $(this).find('input[name="ageUser"]').val();
+                        var password = $(this).find('input[name="passwordUser"]').val();
+                        var verifPass = $(this).find('input[name="verifPasswordUser"]').val();
+                        var formValid = true;
+                        var response = "Pseudo trop court";
+
+                        if (name.length < 5) {
+                            $('#error-inscription').html(response);
+                            return false;
+                        };
+                        
+                        if (name.length == "") {
+                            response = "Veuillez saisir un nom";
+                            $('#error-inscription').html(response);
+                            return false;
+                        };
+
+                        if (firstName.length == "" || firstName.length < 5) {
+                            response = "Veuillez sasir un prenom";
+                            $('#error-inscription').html(response);
+                            return false;
+                        };
+
+                        if (password.length == "" || verifPass.length == "") {
+                            response = "Veuillez saisir un mot de passe";
+                            $('#error-inscription').html(response);
+                            return false;
+                        };
+
+                        if (password != verifPass) {
+                            response = "Mot de passe non identiques";
+                            $('#error-inscription').html(response);
+                            return false;
+                        };
+
+                        if (!isNaN(age) === true) {
+                            response = "Veuillez entrer un entier pour votre age";
+                            $('#error-inscription').html(response);
+                            return false;
+                        };
+                        
+                        return false;
+                });
+        });
+    </script>
     <script type="text/javascript">
         $(document).ready(function() {
             // Using custom configuration
